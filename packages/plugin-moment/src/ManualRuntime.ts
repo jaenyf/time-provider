f0c0b259f925ca3a81e012557c974a5ecaed2c61
jaenyf <95911656+jaenyf@users.noreply.github.com>
2026-07-16 22:16:29 +0200

    fix(core,plugins): atomicity of manual advance
    This fix some manual advance calls resulting in incorrect triggers of scheduled callback
    
    Fixes: #56

--- packages/plugin-moment/src/ManualRuntime.ts
+++ packages/plugin-moment/src/ManualRuntime.ts
@@ -1,41 +1,29 @@
-import {
-  type IAdvanceConfiguration,
-  BaseManualRuntime,
-  type IManualRuntime,
-} from "@time-provider/core";
+import { BaseManualRuntime } from "@time-provider/core";
 
 import { RuntimeHelper } from "./RuntimeHelper.ts";
 import moment from "moment";
 
 export class ManualRuntime extends BaseManualRuntime<moment.Moment> {
-  private ensureDeterminedTime(time: moment.Moment) {
-    this.setDeterminedTime(moment(time.toISOString()));
+  protected advanceYears(time: moment.Moment, years: number): moment.Moment {
+    return time.add({ years });
   }
-  override advanceImpl(advanceConfiguration: IAdvanceConfiguration): IManualRuntime<moment.Moment> {
-    if (advanceConfiguration.days) {
-      this.ensureDeterminedTime(this.utcNow().add({ days: advanceConfiguration.days }));
-    }
-    if (advanceConfiguration.hours) {
-      this.ensureDeterminedTime(this.utcNow().add({ hour: advanceConfiguration.hours }));
-    }
-    if (advanceConfiguration.milliseconds) {
-      this.ensureDeterminedTime(
-        this.utcNow().add(advanceConfiguration.milliseconds, "milliseconds"),
-      );
-    }
-    if (advanceConfiguration.minutes) {
-      this.ensureDeterminedTime(this.utcNow().add(advanceConfiguration.minutes, "minutes"));
-    }
-    if (advanceConfiguration.months) {
-      this.ensureDeterminedTime(this.utcNow().add(advanceConfiguration.months, "months"));
-    }
-    if (advanceConfiguration.seconds) {
-      this.ensureDeterminedTime(this.utcNow().add(advanceConfiguration.seconds, "seconds"));
-    }
-    if (advanceConfiguration.years) {
-      this.ensureDeterminedTime(this.utcNow().add(advanceConfiguration.years, "years"));
-    }
-    return this;
+  protected advanceMonths(time: moment.Moment, months: number): moment.Moment {
+    return time.add({ months });
+  }
+  protected advanceDays(time: moment.Moment, days: number): moment.Moment {
+    return time.add({ days });
+  }
+  protected advanceHours(time: moment.Moment, hours: number): moment.Moment {
+    return time.add({ hours });
+  }
+  protected advanceMinutes(time: moment.Moment, minutes: number): moment.Moment {
+    return time.add({ minutes }).clone();
+  }
+  protected advanceSeconds(time: moment.Moment, seconds: number): moment.Moment {
+    return time.add({ seconds });
+  }
+  protected advanceMilliseconds(time: moment.Moment, milliseconds: number): moment.Moment {
+    return time.add({ milliseconds });
   }
   protected convertToTimestampImpl(time: string | number | moment.Moment): number {
     return RuntimeHelper.convertToTimestamp(time);
