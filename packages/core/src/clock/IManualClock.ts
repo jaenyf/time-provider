6bc8d103f802a249c37e66369a2a712699384900
jaenyf <95911656+jaenyf@users.noreply.github.com>
2026-07-18 21:35:00 +0200

    refactor(core): fix the order of manual time advancement and document it

--- packages/core/src/clock/IManualClock.ts
+++ packages/core/src/clock/IManualClock.ts
@@ -1,5 +1,13 @@
 import type { IClock } from "./IClock.ts";
 
+/**
+ * Describe the time elements to manually advance.
+ *
+ * Note: When more than one element is set, they are applied to the current time in a
+ * fixed order :  years, months,days, hours, minutes, seconds, milliseconds.
+ * This order is important because, for calendar-variable elements (`months`, `years`), combining them with other
+ * elements can give a different result than a different application order would.
+ */
 export interface IAdvanceConfiguration {
   hours?: number;
   minutes?: number;
