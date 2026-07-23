942952f839c4dc45e0afeaabf8b77251fb8fa111
jaenyf <95911656+jaenyf@users.noreply.github.com>
2026-07-23 12:05:42 +0200

    chore: fix formating in test files

--- packages/test-shared/src/testFixedRuntime.ts
+++ packages/test-shared/src/testFixedRuntime.ts
@@ -14,7 +14,10 @@ export function testFixedRuntime<TDate>(
   parseTimeToUtc: (initialValue: string | number | TDate) => TDate,
   parseTimeToLocal: (initialValue: string | number | TDate) => TDate,
 ) {
-  const createFixedRuntime = (timezone: TimezoneDefinition, initialTime: string | number | TDate) =>
+  const createFixedRuntime = (
+    timezone: TimezoneDefinition,
+    initialTime: string | number | TDate,
+  ) =>
     plugin.supportsLocalTime
       ? plugin.createFixedRuntime(timezone, initialTime)
       : plugin.createFixedRuntime(initialTime);
@@ -43,7 +46,29 @@ export function testFixedRuntime<TDate>(
       testScheduler(() => createSUT().scheduler, true);
       describe("issue#57", () => {
         //see: https://github.com/jaenyf/time-provider/issues/57
-        test.each([0, -1, -100])("a zero-or-negative-delay timeout should not fire", (delay) => {
+        test.each([0, -1, -100])(
+          "a zero-or-negative-delay timeout should not fire",
+          (delay: number) => {
+            const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
+            let timeoutCalled = false;
+            sut.scheduler.setTimeout(() => {
+              timeoutCalled = true;
+            }, delay);
+            expect(timeoutCalled).toBe(false);
+          },
+        );
+        test.each([0, -1, -100])(
+          "a zero-or-negative-delay interval should not fire",
+          (delay: number) => {
+            const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
+            let intervalCalled = false;
+            sut.scheduler.setInterval(() => {
+              intervalCalled = true;
+            }, delay);
+            expect(intervalCalled).toBe(false);
+          },
+        );
+        test.each([1, 2, 100])("a positive-delay timeout should not fire", (delay: number) => {
           const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
           let timeoutCalled = false;
           sut.scheduler.setTimeout(() => {
@@ -51,23 +76,7 @@ export function testFixedRuntime<TDate>(
           }, delay);
           expect(timeoutCalled).toBe(false);
         });
-        test.each([0, -1, -100])("a zero-or-negative-delay interval should not fire", (delay) => {
-          const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
-          let intervalCalled = false;
-          sut.scheduler.setInterval(() => {
-            intervalCalled = true;
-          }, delay);
-          expect(intervalCalled).toBe(false);
-        });
-        test.each([1, 2, 100])("a positive-delay timeout should not fire", (delay) => {
-          const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
-          let timeoutCalled = false;
-          sut.scheduler.setTimeout(() => {
-            timeoutCalled = true;
-          }, delay);
-          expect(timeoutCalled).toBe(false);
-        });
-        test.each([1, 2, 100])("a positive-delay interval should not fire", (delay) => {
+        test.each([1, 2, 100])("a positive-delay interval should not fire", (delay: number) => {
           const sut = createFixedRuntime("Pacific/Kiritimati", "2026-01-01T00:00:00.000Z");
           let intervalCalled = false;
           sut.scheduler.setInterval(() => {
