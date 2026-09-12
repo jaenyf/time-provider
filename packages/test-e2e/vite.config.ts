8ab1ef882f468737b72527c9470f23370e052956
jaenyf <95911656+jaenyf@users.noreply.github.com>
2026-09-12 16:13:24 +0200

    chore: prevent test-e2e from autogenerating inlinedDependencies in package.json

--- packages/test-e2e/vite.config.ts
+++ packages/test-e2e/vite.config.ts
@@ -1,2 +1,10 @@
 import packageConfig from "../../vite.packages.config.ts";
-export default packageConfig;
+
+export default {
+  ...packageConfig,
+  pack: {
+    // oxlint-disable-next-line typescript/no-misused-spread
+    ...packageConfig.pack,
+    exports: { inlinedDependencies: false },
+  },
+};
